c121c5103788f453c036ca7fca8168ef7f1ea34c
Nurullah Sevinçtekin <nsevinctekin@gmail.com>
2020-12-25 12:15:49 +0300

    Initial.

---
+++ embed-react-build-in-wordpress.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Embed React Build in Wordpress
+ * Plugin URI: https://github.com/nurullah/embed-react-build-in-wordpress
+ * Description: This plugin enables <strong>ReactJS</strong> builds to work in wordpress.
+ * version: 0.1.0
+ * Author: Nurullah Sevinctekin
+ * Author URI: https://github.com/nurullah/
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
